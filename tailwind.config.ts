2181617d627ffe1317c4a6c1efaca0eb32480a49
David Benton <djb@dajabe.nz>
2023-04-02 17:21:58 +1200

    Style front page with a form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          takaro: "#ed771f",
+          shaded: "#e88235",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
