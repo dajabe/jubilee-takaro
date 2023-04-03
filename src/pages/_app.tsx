969b6697707f8deedff12f3ddb55f2338247d5d0
David Benton <djb@dajabe.nz>
2023-04-03 16:25:02 +1200

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,17 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppProps, type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
+// import { type Session } from "next-auth";
+// import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
 import Form from "./form";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const secretMessage = api.example.getSecretMessage.useQuery();
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const secretMessage = api.example.getSecretMessage.useQuery();
+
+  const user = useUser();
 
   return (
     <>
@@ -41,33 +42,25 @@ const Home: NextPage = () => {
             <Form />
           </div>
         </div>
+        <div>
+          {!user.isSignedIn && (
+            <SignInButton mode="modal">
+              <button className="focus:shadow-outline rounded bg-gray-200 px-4 py-2 font-bold text-orange-takaro shadow hover:bg-white focus:outline-none">
+                Sign In
+              </button>
+            </SignInButton>
+          )}
+          {user.isSignedIn && (
+            <SignOutButton>
+              <button className="focus:shadow-outline rounded bg-gray-200 px-4 py-2 font-bold text-orange-takaro shadow hover:bg-white focus:outline-none">
+                Sign Out
+              </button>
+            </SignOutButton>
+          )}
+        </div>
       </main>
     </>
   );
 };
 
 export default Home;
-
-// const AuthShowcase: React.FC = () => {
-//   const { data: sessionData } = useSession();
-
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// };
