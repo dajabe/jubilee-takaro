b2aec8ac74dd945ca3efdb9df2714083d035509b
David Benton <djb@dajabe.nz>
2023-05-04 14:12:12 +1200

    change background image to start at center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[theme('colors.orange.takaro')] to-[#b47834]">
-        <div className="absolute left-0 top-0 h-full w-full bg-opacity-60 bg-[url('/takaro-pattern-150.png')] bg-repeat "></div>
+        <div className="absolute left-0 top-0 h-full w-full bg-opacity-30 bg-[url('/takaro-pattern-150.png')] bg-center bg-repeat "></div>
         <div className="z-10 flex w-screen flex-col items-center justify-center gap-6 px-4 py-16">
           <div className="container flex flex-col items-center justify-center gap-4">
             <div className="flex flex-col items-center gap-2">
