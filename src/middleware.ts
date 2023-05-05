54c8a414b90be9e9a098851687dfaa7637145cb9
David Benton <djb@dajabe.nz>
2023-05-06 00:35:26 +1200

    updated clerk and added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,32 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { type NextRequest, NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import { type NextApiRequest, type NextApiResponse } from "next";
 
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
+// export default withClerkMiddleware((req: NextRequest) => {
+//   return NextResponse.next();
+// });
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+
+const isPublic = (path: string) => {
+  return publicPaths.some((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default authMiddleware((req) => {
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    const signInUrl = new URL("/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_to", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextRequest.next();
 });
 
 export const config = {
